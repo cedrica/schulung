4db878d6e4b5cf4f9ce564211d85df2d532e7241
cedric <djikeussi2001@yahoo.fr>
2019-10-04 16:49:10 +0200

    profile, properties und file inclusion bzw exclusion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,10 @@
-import de.sdvrz.kbs.data.Account;
-import de.sdvrz.kbs.exception.CustomException;
-import de.sdvrz.kbs.service.AccountService;
-import org.apache.commons.text.StringEscapeUtils;
-
-import java.util.Arrays;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
-        AccountService accountService = new AccountService();
+       /* AccountService accountService = new AccountService();
         try {
             accountService.findAccount(5, null);
         } catch (CustomException e) {
@@ -15,6 +12,29 @@ public class Main {
         }
 
         String json = StringEscapeUtils.escapeJson("{annna:1}");
-        accountService.findAccount2(2,null );
+        accountService.findAccount2(2,null );*/
+
+
+        Properties prop = new Main().loadPropertiesFile("prod.properties");
+        System.out.printf(prop.get("java.version").toString());
+    }
+
+    public void foo(){
+
+    }
+
+    public Properties loadPropertiesFile(String filePath) {
+
+        Properties prop = new Properties();
+
+        try  {
+            InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(filePath);
+            prop.load(resourceAsStream);
+        } catch (IOException e) {
+            System.err.println("Unable to load properties file : " + filePath);
+        }
+
+        return prop;
+
     }
 }
